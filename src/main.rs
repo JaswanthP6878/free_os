311863a97ccd34eb3d18a331e89d4cbfd2397cbd
JaswanthP6878 <klucse2000039034@gmail.com>
2024-09-24 00:48:52 +0530

    started adding testing features

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()])  {
+    println!("Runnuing {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -14,10 +28,22 @@ fn panic(info: &PanicInfo) -> ! {
 
 static HELLO: &[u8] = b"Hello World";
 
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Something went wrong");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
 
